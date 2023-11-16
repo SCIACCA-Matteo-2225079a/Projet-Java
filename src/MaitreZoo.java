510c3a4634494fd9fd3e11ddaf73713942f7ab8b
MaoulidaRaoul <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-11-16 16:39:09 +0100

    ajout classe

--- src/MaitreZoo.java
+++ src/MaitreZoo.java
@@ -1,3 +1,43 @@
 public class MaitreZoo{
+    private String nom;
+    private String sexe;
+    private int age;
 
+    public MaitreZoo()
+    {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public examinerEnclos()
+    {}
+    public nettoyerenclos()
+    {}
+    public  nourrir()
+    {}
+    public transfer()
+    {}
+    public seDeplacer()
+    {}
 }
