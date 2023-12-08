1cb99fc880c4d7edd8441e7f8227d32f4899a619
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2023-12-08 22:06:27 +0100

    Javadoc partie 5

--- src/main/java/modele/Voliere.java
+++ src/main/java/modele/Voliere.java
@@ -6,19 +6,41 @@ public class Voliere extends Enclos {
 
     private double hauteur;
 
+    /**
+     * Caractéristiques de la voliére
+     * @param nom
+     * @param superficie
+     * @param nbMaxCreatures
+     * @param nbCreatures
+     * @param creaturesPres
+     * @param proprete
+     * @param hauteur
+     */
     public Voliere(String nom, double superficie, int nbMaxCreatures, int nbCreatures, ArrayList<Creature> creaturesPres, int proprete, double hauteur) {
         super(nom, superficie, nbMaxCreatures, creaturesPres, proprete);
         this.hauteur = hauteur;
     }
 
+    /**
+     * Récupérer la hauteur de la voliére
+     * @return hauteur
+     */
     public double getHauteur() {
         return hauteur;
     }
 
+    /**
+     * Modifier l'hauteur de la voliére
+     * @param hauteur
+     */
     public void setHauteur(double hauteur) {
         this.hauteur = hauteur;
     }
 
+    /**
+     * Affichage caractéristiques de la voliére
+     * @return Caractéristiques de la voliére
+     */
     @Override
     public String afficherCaracteristiques() {
         return "Nom : " + getNom() +
@@ -28,6 +50,10 @@ public class Voliere extends Enclos {
                 ", Hauteur de la volière : " + getHauteur();
     }
 
+    /**
+     * Affichage hauteur de la voliére
+     * @return toString
+     */
     @Override
     public String toString() {
         return super.toString() + " Hauteur de la volière : " + getHauteur();
