426ff3118a1e64ef9f29a1f2b785b22c07f0b439
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2023-12-08 22:10:52 +0100

    Javadoc partie 6

--- src/main/java/modele/Zoo.java
+++ src/main/java/modele/Zoo.java
@@ -15,6 +15,15 @@ public class Zoo  {
     private int nbMaxEnclos;
     private int affichernbCreaturesZoo;
 
+    /**
+     * Caractéristiques du zoo
+     * @param nom
+     * @param maitre
+     * @param nbCreaturesZoo
+     * @param creaturePres
+     * @param enclosExist
+     * @param nbMaxEnclos
+     */
     public Zoo(String nom, MaitreZoo maitre , int nbCreaturesZoo, ArrayList<Creature> creaturePres, ArrayList<Enclos> enclosExist, int nbMaxEnclos)
     {
 
@@ -28,25 +37,45 @@ public class Zoo  {
         //this.jour = jour;
     }
 
+    /**
+     * Modifier les créatures présentes dans le zoo
+     * @param creaturePres
+     */
     public void setCreaturePres(ArrayList<Creature> creaturePres) {
         this.creaturePres = creaturePres;
     }
 
 
-
+    /**
+     * Récupérer le nom du zoo
+     * @return nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Modifier le nom du zoo
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Récupérer le nom du maitre du zoo
+     * @param maitre
+     * @return maitre.getNom
+     */
    public String maitre(MaitreZoo maitre) {
 
        return maitre.getNom();
    }
 
+    /**
+     * Récupérer numéro de créatures dans le zoo
+     * @return nbCreaturesZoo
+     */
     public int getNbCreaturesZoo() {
         int nbCreaturesZoo = 0;
 
@@ -58,20 +87,31 @@ public class Zoo  {
     }
 
 
-
+    /**
+     * Nombre de créatures dans le zoo
+     * @param nbCreaturesZoo
+     */
     public void setNbCreaturesZoo(int nbCreaturesZoo) {
 
         this.nbCreaturesZoo = nbCreaturesZoo;
     }
 
 
-
+    /**
+     * Affichage nombre de créatures dans le zoo
+     * @return nbCreaturesZoo
+     */
     public int afficherNbCreaturesZoo() {
 
         return nbCreaturesZoo;
 
     }
 
+    /**
+     * Liste créatures présentes dans le zoo
+     * @param creaturePresZoo
+     * @return creaturePresZoo
+     */
     public ArrayList<Creature> afficherCreaturesPres(ArrayList<Creature> creaturePresZoo) {
         // Tri par sélection
         trierParOrdreAlphabetique(creaturePresZoo);
@@ -84,6 +124,10 @@ public class Zoo  {
         return creaturePresZoo;
     }
 
+    /**
+     * Tri par ordre alphabétique
+     * @param creatures
+     */
     public void trierParOrdreAlphabetique(ArrayList<Creature> creatures) {
         Collections.sort(creatures, new Comparator<Creature>() {
             @Override
@@ -94,12 +138,18 @@ public class Zoo  {
         });
     }
 
+    /**
+     * Incrémentation des jours
+     * @return jour
+     */
     public static int incrementerJour() {
         jour++;
         return jour;
     }
 
-
+    /**
+     * Lancement du timer
+     */
     public static void lancerTimer() {
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -110,15 +160,28 @@ public class Zoo  {
         }, 10*1000, 10*1000); // Le deuxième argument est le délai initial, le troisième est l'intervalle en millisecondes
     }
 
+    /**
+     * Affichage du jour
+     * @return jour
+     */
     public static int getJour() {
 
         return jour;
     }
 
+    /**
+     * Récupératio du numéro maximal d'enclos
+     * @return
+     */
     public int nbMaxEnclos() {
 
         return nbMaxEnclos;
     }
+
+    /**
+     * Ajout d'un enclos
+     * @param enclos
+     */
     public void ajouterEnclos(Enclos enclos) {
         // Vérifier si l'enclos a atteint sa capacité maximale
         if (enclosExist.size() < nbMaxEnclos) {
@@ -133,6 +196,11 @@ public class Zoo  {
             System.out.println("Le Zoo est plein, impossible d'ajouter une nouvel  enclos.");
         }
     }
+
+    /**
+     * Message de bienvenue au zoo
+     * @return toString
+     */
     @Override
     public String toString() {
         return "Bienvenue à " + nom + ". Nombre d'enclos : "+ enclosExist.size();
