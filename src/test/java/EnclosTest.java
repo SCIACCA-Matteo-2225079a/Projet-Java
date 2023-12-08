e01254e6535c264935f11358ad93d9b6c61a5208
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2023-12-08 21:04:05 +0100

    Modification tests

--- src/test/java/EnclosTest.java
+++ src/test/java/EnclosTest.java
@@ -10,28 +10,6 @@ class EnclosTest {
 
 
     @Test
-    void testAjouterCreature() {
-        Enclos enclos = new Enclos("EnclosTest", 100.0, 3, new ArrayList<>(), 8);
-        Creature creature = new Creature("Phénix", "Male", 50, 150, 5, true, true, false, 100, false,"Hiiii");
-
-        enclos.ajouterCreature(creature);
-
-        assertTrue(enclos.getCreaturesPres().contains(creature));
-        assertEquals(1, enclos.getNbCreatures());
-    }
-
-    @Test
-    void testEnleverCreature() {
-        Enclos enclos = new Enclos("Enclos Test", 100.0, 3, new ArrayList<>(), 8);
-        Creature creature = new Creature("Licornes", "Male", 50, 150, 5, true, true, false, 100, false,"Hiiii");
-
-            enclos.ajouterCreature(creature);
-            enclos.enleverCreature(creature);
-
-            assertFalse(enclos.getCreaturesPres().contains(creature));
-    }
-
-    @Test
     void testContientCreature() {
         Enclos enclos = new Enclos("Enclos Test", 100.0, 3, new ArrayList<>(), 8);
         Creature creature = new Creature("Mégalodon", "Male", 50, 150, 5, true, true, false, 100, false,"Hiiii");
